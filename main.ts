2778395cc41cc79af58630c166cd6c37da136f2e
Rodolfo <todocono@gmail.com>
2022-12-21 15:51:18 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+radio.onReceivedValue(function (name, value) {
+    if (name == "x") {
+        modules.hidMouse1.move(value, 0)
+    } else if (name == "y") {
+        modules.hidMouse1.move(0, value)
+    } else if (name == "adown") {
+        modules.hidMouse1.setButton(jacdac.HidMouseButton.Left, jacdac.HidMouseButtonEvent.Down)
+    } else if (name == "aup") {
+        modules.hidMouse1.setButton(jacdac.HidMouseButton.Left, jacdac.HidMouseButtonEvent.Up)
+    } else {
+    	
+    }
+})
+radio.setGroup(1)
+basic.forever(function () {
+	
+})
